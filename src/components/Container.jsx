f4ad13a059474bdfdc892c62edecb2fc77c1618e
Ravend1028 <ravendavid.rd30@gmail.com>
2024-10-24 23:23:58 +0800

    add env variable and finished api call

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,10 +1,32 @@
-import React, { useEffect } from 'react'
-import { useState } from 'react'
+import React from 'react'
+import { useState, useEffect } from 'react'
+import Spinner from './Spinner'
 import Card from './Card'
 
 const Container = () => {
+  const [comics, setComics] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchComics = async () => {
+      const apiUrl = '';
+      try {
+        const res = await fetch(apiUrl);
+        const data = await res.json();
+        setComics(data);
+      } catch (error) {
+        console.log('Error fetching data', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchComics();
+  }, []);
+
   return (
     <main className='container mx-auto p-6 grid grid-cols-4'>
+      {/* Handle Fetched Data Here */}
     </main>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import logo from '../assets/images/marvel-logo.png'
+import { FaSearch } from "react-icons/fa"
 
 const Header = () => {
   return (
@@ -8,10 +9,18 @@ const Header = () => {
         {/* <img className='w-20' src={ logo } alt="marvel-logo"/> */}
 
         <h3 className='text-2xl text-white'>
-          Basta Gawa ito ni Raven 123 Hahahah
+          Basta Gawa ito ni Raven Pota Hahahah
         </h3>
 
-        <input className='p-2 rounded-lg w-1/6' type="search" placeholder='Search Comic Title'/>
+        <div className="relative w-1/6">
+          <input
+            type="search"
+            className="p-2 pl-10 rounded-lg w-full border border-gray-300"
+            placeholder="Search Comic Title"
+          />
+          
+          <FaSearch className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
+        </div>
       </div>
     </header>
   )
